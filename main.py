f2828dc21ddc74b1df1cf408cff5e9ecc9bff8cc
Emily Dawson <lemilydawson@gmail.com>
2023-07-30 17:40:12 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+user_prompt = "Enter a todo:" # good practice to keep variables outside of loop since it will be 
+
+todos = []
+
+while True : 
+    todo = input(user_prompt)
+    print(todo.title()) # methods . notation  AND () | can take arguements
+    todos.append(todo) # method
+    
+
+    
+# numbers = []
+# number = 0 
+# n = 0 
+
+# while n < 6:
+#     n = n + 1 
+#     print(n)
+#     numbers.append(n)
+#     number = n + 1 
+    
+    
+# print(numbers)
+# print(number)
+
+    
+
+
+    
+    
+    
